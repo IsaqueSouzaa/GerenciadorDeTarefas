eaa24af7b2a0405a60991e60b55a41ce7b62dde8
IsaqueSouzaa <isaquebarbosa463@gmail.com>
2026-01-06 03:18:27 -0300

    Terminei de programar a Tasks

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,31 @@
+import { ChevronRightIcon, Trash2 } from "lucide-react";
+
+function Tasks(props) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 w-full text-left text-white p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 text-white rounded-md">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => props.onDeleteTaskClick(task.id)}
+            className="bg-slate-400 p-2 text-white rounded-md"
+          >
+            <Trash2 />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
