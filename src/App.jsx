4c2dda90f805c88c2a5c3e911c682513220568b2
IsaqueSouzaa <isaquebarbosa463@gmail.com>
2026-01-07 08:39:30 -0300

    Configurei o AddTasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Tasks from "./components/Tasks";
+import AddTasks from "./components/AddTasks";
+import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -38,13 +40,23 @@ function App() {
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
   }
+
+  function onAddTaskSubmit(title, description) {
+    const newTasks = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTasks]);
+  }
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-200 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-
+        <AddTasks onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks
           tasks={tasks}
           onTaskClick={onTaskClick}
--- src/components/AddTasks.jsx
+++ src/components/AddTasks.jsx
@@ -1,5 +1,40 @@
-function AddTasks() {
-  return <h1>AddTasks</h1>;
+import { useState } from "react";
+
+function AddTasks({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div className="bg-slate-200 space-y-4 p-6 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o titulo da tarefa"
+        className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+
+      <button
+        onClick={() => {
+          if (!title || !description) {
+            return alert("Preencha o titulo e a descrição");
+          }
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="bg-slate-500 rounded-md text-white px-4 py-2 font-medium "
+      >
+        Adicionar
+      </button>
+    </div>
+  );
 }
 
 export default AddTasks;
